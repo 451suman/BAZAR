b8d4e7ccb81f9102a8322ea74a588519e4097974
451suman <sumanmushyakhwo@gmail.com>
2024-09-28 16:58:48 +0545

    product is display in list

--- bazar_app/urls.py
+++ bazar_app/urls.py
@@ -1,9 +1,8 @@
-
 from django.urls import path
 
 from bazar_app import views
 
 urlpatterns = [
-path("",views.HomeView.as_view(), name="home"),
-
+    path("", views.HomeView.as_view(), name="home"),
+    path("product-list/", views.ProductsListView.as_view(), name="product-list")
 ]
--- bazar_app/views.py
+++ bazar_app/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, DeleteView, UpdateView, ListView, DetailView
+from django.views.generic import (
+    TemplateView,
+    CreateView,
+    DeleteView,
+    UpdateView,
+    ListView,
+    DetailView,
+)
 
 from bazar_app.models import Product
+
+
 # Create your views here.
 class HomeView(ListView):
     model = Product
-    template_name = 'home/home.html'
-    context_object_name = 'products'
+    template_name = "home/home.html"
+    context_object_name = "products"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,3 +28,15 @@ class HomeView(ListView):
         ).order_by("-published_at")
 
         return context
+
+
+class ProductsListView(ListView):
+    model = Product
+    template_name = "product_list/product_list.html"
+    context_object_name = "products"
+    paginate_by = 9
+
+    def get_queryset(self):
+        query = super().get_queryset()
+        query = Product.objects.filter(published_at__isnull=False, status="active").order_by("-published_at")
+        return query
